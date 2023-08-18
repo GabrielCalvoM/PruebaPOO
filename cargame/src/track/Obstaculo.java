94c0384f26dd0b3796c85c1e5568c0ac01892cca
unknown <gabriel.g.calvo.m@gmail.com>
2023-08-18 11:50:10 -0600

    Mensaje Prueba

--- cargame/src/track/Obstaculo.java
+++ cargame/src/track/Obstaculo.java
@@ -1,5 +1,14 @@
 package track;
 
 public class Obstaculo {
+         
+    private int tipo;
+    private int daño;
+    private int tamaño;
+
+    public Obstaculo(tipo);{
+        daño = (tipo % 4 + 2) * 3;
+        tamaño = tipo % 3 + 1;
+    }
 
 }
